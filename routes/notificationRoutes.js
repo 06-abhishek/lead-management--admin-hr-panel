0509d85ce62dd65c56d30f4ef3515bb39a9ed4d2
Abhishek Patil <abhishekp.7841@gmail.com>
2025-03-03 20:33:47 +0530

    Debbuged code

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -6,7 +6,11 @@ router.get("/", notificationController.getAllNotifications);
 router.get("/unread", notificationController.getUnreadNotifications);
 router.put("/mark-read/:id", notificationController.markAsRead);
 router.delete("/:id", notificationController.deleteNotification);
+
 router.get("/priority-alerts", notificationController.getPriorityAlerts);
+router.put("/account-request/:id", notificationController.processAccountRequest);
+router.put("/deactivation-request/:id", notificationController.processDeactivationRequest);
+
 router.get("/history", notificationController.getNotificationHistory);
 router.get("/system-status", notificationController.getSystemStatus);
 
